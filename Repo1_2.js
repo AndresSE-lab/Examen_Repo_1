7d1dbda9dfd17b238f766fe240115500c1fd8cc6
Andres Salcedo Elizondo <andresase05@gmail.com>
2025-06-03 11:07:51 -0400

    Subi el primer alrchivo del segundo repo

--- Repo1_2.js
+++ Repo1_2.js
@@ -2,3 +2,4 @@
 'Los sistemas op, vienen con pragramas especificos de cada uno, y hay uno en especial que es el que tiene licencias y es el que te pide una membresia'
 'La mayoria ya vienen con un instalador automatico de controladores y drivers al momento que tu descargas o agregas un dispositivo a tu pc'
 
+'git remote add origin https://github.com/AndresSE-lab/Examen_Repo_2.git'
