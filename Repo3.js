b52b1884a2f1265ef6d0fedde3a87d5e6bb16e1b
AndresSE-lab <andresase05@gmail.com>
2025-06-03 11:57:33 -0400

    Delete Repo3.js

--- Repo3.js
+++
@@ -1,2 +0,0 @@
-'Los sistemas op, vienen con pragramas especificos de cada uno, y hay uno en especial que es el que tiene licencias y es el que te pide una membresia'
-'La mayoria ya vienen con un instalador automatico de controladores y drivers al momento que tu descargas o agregas un dispositivo a tu pc'
