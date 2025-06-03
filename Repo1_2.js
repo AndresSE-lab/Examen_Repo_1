3ae09b0173cc7d54b7d195e4e1d289a982af83c2
Andres Salcedo Elizondo <andresase05@gmail.com>
2025-06-03 11:04:14 -0400

    Subi el primero archivo del segundo

---
+++ Repo1_2.js
@@ -0,0 +1,4 @@
+'Estos sistemas viene con sus ventajas y desventajas, ya que cada uno tiene un enfoque un poco distinto'
+'Los sistemas op, vienen con pragramas especificos de cada uno, y hay uno en especial que es el que tiene licencias y es el que te pide una membresia'
+'La mayoria ya vienen con un instalador automatico de controladores y drivers al momento que tu descargas o agregas un dispositivo a tu pc'
+
